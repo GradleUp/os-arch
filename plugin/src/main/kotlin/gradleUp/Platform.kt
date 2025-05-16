538b19a222c00a5ff249dce9ae21fafca7f98e2f
Giuseppe Barbieri <elect86@gmail.com>
2025-05-16 05:22:11 +0200

    Fix spacing in Platform.kt and add a classifier property
    Corrected spacing in the Platform data class for better consistency. Added a `classifier` property to provide a useful identifier. Bumped the plugin version from 0.1.0 to 0.1.1 to reflect these changes.

--- plugin/src/main/kotlin/gradleUp/Platform.kt
+++ plugin/src/main/kotlin/gradleUp/Platform.kt
@@ -3,13 +3,15 @@ package gradleUp
 import org.gradle.api.Named
 import org.gradle.api.attributes.Attribute
 
-data class Platform(val os: OS, val arch: Arch): Named {
+data class Platform(val os: OS, val arch: Arch) : Named {
 
     constructor(pair: Pair<OS, Arch>) : this(pair.first, pair.second)
 
     override fun getName() = "$os-$arch"
 
     override fun toString() = name
+
+    val classifier = "natives-$name"
 
     companion object {
         val ATTRIBUTE = Attribute.of(Platform::class.java)
