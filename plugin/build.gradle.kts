fe8b39a499abdae96c8c02a559b728137cf23b4d
Giuseppe Barbieri <elect86@gmail.com>
2025-05-16 05:09:19 +0200

    Add runtime configuration attributes for OS and architecture
    This commit updates the `OsArchPlugin` to set OS and architecture attributes for configurations containing "runtime" in their name. It ensures better platform-specific resolution by leveraging Gradle's `OperatingSystemFamily` and `MachineArchitecture` attributes. Additionally, the plugin version is incremented from 0.0.8 to 0.0.9.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "io.github.gradleUp"
-version = "0.0.8"
+version = "0.0.9"
 
 repositories {
     // Use Maven Central for resolving dependencies.
--- plugin/src/main/kotlin/gradleUp/OsArchPlugin.kt
+++ plugin/src/main/kotlin/gradleUp/OsArchPlugin.kt
@@ -2,7 +2,20 @@ package gradleUp
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.nativeplatform.MachineArchitecture
+import org.gradle.nativeplatform.OperatingSystemFamily
 
-class OsArchPlugin: Plugin<Project> {
-    override fun apply(project: Project) {}
+class OsArchPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        project.configurations.configureEach {
+            if ("runtime" in name) {
+                println("setting attributes for '$name' configuration")
+                attributes {
+                    val platform = Platform.current
+                    attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, project.objects.named(platform.os))
+                    attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, project.objects.named(platform.arch))
+                }
+            }
+        }
+    }
 }
